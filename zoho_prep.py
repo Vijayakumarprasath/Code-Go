0d238682c40059d55b5b2fc5458fe61baf0f9242
Vijayaprasath <srivijayaprasath@gmail.com>
2025-12-12 12:33:30 +0530

    feat : both side change

--- zoho_prep.py
+++ zoho_prep.py
@@ -6,3 +6,8 @@ def twoSum(nums, target):
         if target - n in mp:
             return [mp[target - n], i]
         mp[n] = i
+
+
+#3 Contains Duplicate
+def containsDuplicate(nums):
+    return len(nums) != len(set(nums))
